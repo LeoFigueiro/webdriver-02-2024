ac85f5cfb7203da11f821382f8f3a12bc70f8305
Antonio Trindade <atrindade@umov.me>
2024-02-29 20:05:11 -0300

    Add Calculadora

---
+++ src/test/java/com/test/CalculadoraTest.java
@@ -0,0 +1,102 @@
+package com.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CalculadoraTest {
+	
+	private WebDriver driver;
+	private WebElement tfNumber1;
+	private WebElement tfNumber2;
+	private WebElement tfTotal;
+	
+	
+	@Before
+	public void setUp() throws Exception {		
+		System.setProperty("webdriver.chrome.driver", "/home/atrindade/Dev/drivers/chromedriver");		
+		driver = new ChromeDriver();
+		driver.get("https://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");
+		Thread.sleep(3000);
+		
+		//mapeia elemento
+		tfNumber1 = driver.findElement(By.xpath("//*[@id='number1']"));				
+		tfNumber2 = driver.findElement(By.id("number2"));
+		tfTotal = driver.findElement(By.id("total"));
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		Thread.sleep(3000);
+		driver.quit();	
+	}
+	
+	@Test
+	public void testSoma() throws InterruptedException {
+		
+		Double valor1 = 20d;
+		Double valor2 = 45.6d;
+		
+		Double totalTest = valor1 + valor2;
+		
+		tfNumber1.sendKeys(Double.toString(valor1));
+		tfNumber2.sendKeys(Double.toString(valor2));
+		
+		WebElement btnSomar = driver.findElement(By.xpath("//input[@value='Somar']"));
+		btnSomar.click();
+		
+		//TODO alterar espera
+		Thread.sleep(3000);
+				
+		String totalCalculadora = tfTotal.getAttribute("value");		
+		assertEquals(Double.toString(totalTest), totalCalculadora);						
+	}
+	
+	@Test
+	public void testSubtracao() {
+		Double valor1 = 60d;		
+		Double valor2 = 45.5d;
+		
+		Double totalTest = valor1 - valor2;
+				
+		tfNumber1.sendKeys(Double.toString(valor1));		
+		tfNumber2.sendKeys(Double.toString(valor2));
+				
+		WebElement btnSutrair = driver.findElement(By.id("subtrair"));
+		btnSutrair.click();		
+		
+		String totalCalculadora = tfTotal.getAttribute("value");
+		System.out.println(totalCalculadora);
+		
+		assertEquals(Double.toString(totalTest), totalCalculadora);
+	}
+	
+	@Test
+	public void testMultiplicacao(){
+		Double valor1 = 60d;		
+		Double valor2 = 3.5d;
+		
+		Double totalTest = valor1 * valor2;
+				
+		tfNumber1.sendKeys(Double.toString(valor1));		
+		tfNumber2.sendKeys(Double.toString(valor2));
+				
+		WebElement btnMultiplicacao = driver.findElement(By.id("multiplicar"));
+		btnMultiplicacao.click();		
+		
+		String totalCalculadora = tfTotal.getAttribute("value");
+		
+		System.out.println(totalTest);
+		System.out.print(Double.parseDouble(totalCalculadora));
+		
+		assertEquals(totalTest, Double.parseDouble(totalCalculadora), 0.00001);		
+	}
+
+}
